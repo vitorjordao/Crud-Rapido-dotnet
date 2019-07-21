b36b0fb94ae11c9e7de55c85ba309a21263737e5
Vitor Jordão <srvitorlol@gmail.com>
2019-07-21 11:16:36 -0300

    Deixando mais condisente a view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,11 +22,6 @@ namespace CrudRapido.Controllers
             return View(pessoas);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,6 @@ namespace CrudRapido
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -45,7 +44,6 @@ namespace CrudRapido
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
